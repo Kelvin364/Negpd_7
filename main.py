d472c53a8d5b3712a526a7f999e054c62ab65c33
Kelvin364 <k.rwihimba@alustudents.com>
2024-07-14 13:28:09 +0200

    commit

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 # main.py
+import tkinter as tk
+from views.main_menu import MainMenuView
 
-from Displays.ui import UI
+def main():
+    root = tk.Tk()
+    root.title("Medical Records System")
+    root.geometry("400x300")
+
+    main_menu_view = MainMenuView(root)
+    main_menu_view.pack(fill="both", expand=True)
+
+    root.mainloop()
 
 if __name__ == "__main__":
-    ui = UI()
-    ui.display_menu()
+    main()
